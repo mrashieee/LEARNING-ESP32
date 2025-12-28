8fb670c1e1dfb39cf94a1e19aee0e5fe2e817735
Muhammad Rashid K <mrashieee@gmail.com>
2025-12-28 22:00:08 +0530

    More detailed Guide about First

--- 1 Introduction/sketch.ino
+++ 1 Introduction/sketch.ino
@@ -3,11 +3,14 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED,OUTPUT);
+  pinMode(LED,OUTPUT);  // pinMode takes (pin number, mode) to use the esp32 pin
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // digitalWrite used for OUTPUT mode devices 
+  // LOW = 0V
+  // HIGH = 3.3V
   digitalWrite(LED,LOW);
   delay(300);     // How long the LED will be ON
   digitalWrite(LED,HIGH);
