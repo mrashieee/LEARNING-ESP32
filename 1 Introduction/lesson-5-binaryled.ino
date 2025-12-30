21163b12da7c7631f5d2469e504ce69b3f31cfea
Muhammad Rashid K <mrashieee@gmail.com>
2025-12-30 17:39:44 +0530

    some more C basic programming

---
+++ 1 Introduction/lesson-5-binaryled.ino
@@ -0,0 +1,276 @@
+
+int LED0 = 12;
+int LED1 = 14;
+int LED2 = 27;
+int LED3 = 26;
+int LED4 = 25;
+int waittime = 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // 0
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 1
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 2
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 3
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 4
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 5
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 6
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 7
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 8
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 9
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 10
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 11
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 12
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 13
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 14
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 15
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 16
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 17
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 18
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 19
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 20
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 21
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 20
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 23
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 24
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 25
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 26
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 27
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 28
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 29
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+
+  // 30
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  delay(waittime);
+
+  // 31
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(waittime);
+}
---
+++ 1 Introduction/lesson-8-redGreenLed.ino
@@ -0,0 +1,80 @@
+
+int redLed = 12;
+int greenLed = 14;
+int delayRed = 1000;
+int delayGreen = 300;
+int delayLong = 1500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(greenLed, HIGH);
+  delay(delayGreen);
+  digitalWrite(greenLed, LOW);
+  delay(delayGreen);
+  digitalWrite(greenLed, HIGH);
+  delay(delayGreen);
+  digitalWrite(greenLed, LOW);
+  delay(delayGreen);
+  digitalWrite(greenLed, HIGH);
+  delay(delayGreen);
+  digitalWrite(greenLed, LOW);
+  delay(delayGreen);
+  digitalWrite(greenLed, HIGH);
+  delay(delayGreen);
+  digitalWrite(greenLed, LOW);
+  delay(delayGreen);
+  digitalWrite(greenLed, HIGH);
+  delay(delayGreen);
+  digitalWrite(greenLed, LOW);
+  delay(delayGreen);
+
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+  digitalWrite(redLed, HIGH);
+  delay(delayRed);
+  digitalWrite(redLed, LOW);
+  delay(delayRed);
+
+  delay(delayLong);
+
+}
