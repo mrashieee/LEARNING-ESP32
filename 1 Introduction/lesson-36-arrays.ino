ba5e14864db7e84e3b568e952af43f246635610f
Muhammad Rashid K <mrashieee@gmail.com>
2026-01-07 22:42:18 +0530

    Day 11 commit

---
+++ 1 Introduction/lesson-36-arrays.ino
@@ -0,0 +1,63 @@
+float grades[25];
+int br = 115200;
+String myPrompt;
+int numGrades;
+int i;
+float sumGrades;
+float averageGrade;
+int highestGrade;
+int lowestGrade;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(br);
+}
+
+void clearSerial() {
+  if (Serial.available()) {
+    Serial.read();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("How many grades did you get?");
+  while (Serial.available() == 0) {
+
+  }
+  numGrades = Serial.parseInt();
+  clearSerial();
+  for (i=0;i<numGrades;i++) {
+    myPrompt = "Please Input Your Grade: "+String(i+1);
+    Serial.println(myPrompt);
+    while (Serial.available() == 0) {
+
+    }
+    grades[i] = Serial.parseFloat();
+    clearSerial();
+  }
+  Serial.println("Your grades are: ");
+  for (i=0;i<numGrades;i++) {
+    Serial.println(grades[i]);
+  }
+  Serial.print("Average Grade is: ");
+  for (i=0;i<numGrades;i++) {
+    sumGrades+=grades[i];
+  }
+  averageGrade = sumGrades/numGrades;
+  Serial.println(averageGrade);
+  for (i=0;i<numGrades;i++) {
+    if (highestGrade < grades[i]) {
+      highestGrade=grades[i];
+    }
+  }
+  lowestGrade = highestGrade;
+  for (i=0;i<numGrades;i++) {
+    if (lowestGrade > grades[i]) {
+      lowestGrade=grades[i];
+    }
+  }
+  Serial.println("Your highest score is: "+String(highestGrade));
+  Serial.println("Your lowest score is: "+String(lowestGrade));
+  clearSerial();
+}
