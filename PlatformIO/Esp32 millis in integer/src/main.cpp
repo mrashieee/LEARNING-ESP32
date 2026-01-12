a14379bcce24c01bd92b461281f47d018590dcd3
Muhammad Rashid K <mrashieee@gmail.com>
2026-01-12 19:59:10 +0530

    Day 14 Commit, module 2 Starts

---
+++ PlatformIO/Esp32 millis in integer/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int hardwareClock;
+int br = 115200;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(br);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  hardwareClock = millis();
+  Serial.println(hardwareClock);
+}
